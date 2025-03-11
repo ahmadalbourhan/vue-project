12f3a8c9226b7b1be59458dccfa490d50cb048f7
ahmadalbourhan <127745237+ahmadalbourhan@users.noreply.github.com>
2025-03-11 10:01:19 +0200

    first commit

---
+++ src/pages/[...catchAll].vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+useErrorStore().setError({ error: 'Page not found', customCode: 404 })
+</script>
+
+<template>
+  <div></div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,70 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import VueRouter from 'unplugin-vue-router/vite'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import Components from 'unplugin-vue-components/vite'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    VueRouter(),
+    Components({
+      /* options */
+    }),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/, // .md
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        VueRouterAutoImports,
+        {
+          pinia: ['defineStore', 'storeToRefs', 'acceptHMRUpdate'],
+        },
+      ],
+
+      // Filepath to generate corresponding .d.ts file.
+      // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
+      // Set `false` to disable.
+      dts: true,
+      // Include auto-imported packages in Vite's `optimizeDeps` options
+      // Recommend to enable
+      viteOptimizeDeps: true,
+      dirs: ['src/stores'],
+    }),
+
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (element) => element.startsWith('iconify-icon'),
+        },
+      },
+    }),
+    vueDevTools(),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
